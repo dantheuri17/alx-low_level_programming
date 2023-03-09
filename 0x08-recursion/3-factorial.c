508f8576d912253f37f465ccdaf0639665732684
Daniel Theuri <dantheuri17@gmail.com>
2023-03-09 23:35:34 +0300

    adding folder

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - finds factorial of a number
+ * @n: number to find factorial of 
+ *
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+		return 1;
+	if (n == 0)
+		return 1;
+	if (n < 0)
+		return -1;
+
+	return n * factorial(n - 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,4 @@
+int _putchar(int n);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+int _strlen_recursion(char *s);
